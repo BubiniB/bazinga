34f9f642c922c62902a7c71cdfe7d3beadff2ed5
BubiniB <cristina.ekqvist@outlook.com>
2023-03-27 08:22:37 +0000

    Get information from the DOM and add to script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,11 @@
-// My two main variables for making the game work
+// Getting information from the DOM
 
-let playerChoice;
-let compChoice;
+let rock = document.getElementById('rock');
+let paper = document.getElementById('paper');
+let scissors = document.getElementById('scissors');
+let lizard = document.getElementById('lizard');
+let spock = document.getElementById('spock');
+
+let playerScore = document.getElementById('player-score');
+let sheldonScore = document.getElementById('sheldon-score');
 
