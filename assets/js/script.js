bdc90215308cd7d96a81e61966eadc57ecd2bb5b
BubiniB <cristina.ekqvist@outlook.com>
2023-03-27 08:37:15 +0000

    Add score functions for player and computer

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,3 +9,20 @@ let spock = document.getElementById('spock');
 let playerScore = document.getElementById('player-score');
 let sheldonScore = document.getElementById('sheldon-score');
 
+
+
+/**
+ * Function to increment player score
+ */
+function incrementPlayerScore() {
+    let currentScore = parseInt(playerScore.innerText);
+    playerScore.innerText = ++currentScore;
+}
+
+/**
+ * Function to increment computer's score
+ */
+function incrementSheldonScore() {
+    let currentScore = parseInt(sheldonScore.innerText);
+    sheldonScore.innerText = ++currentScore;
+}
