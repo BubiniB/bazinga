084fc4ae88b865ddaa0f6bfaafafd13729cf4639
BubiniB <cristina.ekqvist@outlook.com>
2023-03-28 11:05:50 +0000

    Add more info from the DOM to script.js for computer choices and new text areas

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,26 @@
 // Getting information from the DOM
 
+// For creating player choices
 let rock = document.getElementById('rock');
 let paper = document.getElementById('paper');
 let scissors = document.getElementById('scissors');
 let lizard = document.getElementById('lizard');
 let spock = document.getElementById('spock');
 
+// For creating computer choices
+let sheldonRock = document.getElementById('sheldon-rock');
+let sheldonPaper = document.getElementById('sheldon-rock');
+let sheldonScissors = document.getElementById('sheldon-scissors');
+let sheldonLizard = document.getElementById('sheldon-lizard');
+let sheldonSpock = document.getElementById('sheldon-spock');
+
+// For creating text areas, which are going to be filled depending on the game outcome
+let choiceText = document.getElementById('choice-area');
+let resultText = document.getElementById('result-area');
+
+// For the score functions
 let playerScore = document.getElementById('player-score');
 let sheldonScore = document.getElementById('sheldon-score');
-
-let resultText = document.getElementById('result-area');
 
 /**
  * Different choices and rules
